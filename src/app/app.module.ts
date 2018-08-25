9a79b0d5aaff63aed620d331bbfb27f95342e6a6
Matheus Souza <matheussouza@infocast.com.br>
2018-08-25 17:25:11 -0300

    data-bind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { NewMessagesModule  } from './new-messages/new-messages.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CoreModule} from './core'
 @NgModule({
   declarations: [
     AppComponent
@@ -15,6 +15,8 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule,
     AppRoutingModule,
+    CoreModule,
+    NewMessagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
